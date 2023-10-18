f2ba832f7096cffe2cdcd119c48c04c691097962
nicole-cab <nc482@exeter.ac.uk>
2023-10-18 18:19:08 +0100

    react-w3: added 404 page

---
+++ react-tutorial/learn-react/src/components/Page404.jsx
@@ -0,0 +1,9 @@
+function Page404() {
+  return (
+    <>
+      <h1 className="heading">404 Page Not Found</h1>
+    </>
+  );
+}
+
+export default Page404;
--- react-tutorial/learn-react/src/main.jsx
+++ react-tutorial/learn-react/src/main.jsx
@@ -8,6 +8,7 @@ import Events from "./components/Events.jsx";
 import Conditionals from "./components/Conditionals.jsx";
 import Lists from "./components/Lists.jsx";
 import Forms from "./components/Forms.jsx";
+import Page404 from "./components/Page404.jsx";
 import "./index.css";
 // React Router is used in react for page routing
 // (Route attributes): "element" tells you what component to render, "index" specifies this route as the default route for the parent route, e.g. "/"
@@ -33,6 +34,7 @@ root.render(
           <Route path="conditionals" element={<Conditionals />}></Route>
           <Route path="lists" element={<Lists />}></Route>
           <Route path="forms" element={<Forms />}></Route>
+          <Route path="*" element={<Page404 />}></Route>
         </Route>
       </Routes>
     </BrowserRouter>
