0c8ceb402482fe67c9c47514825d127d6792e52a
nicole-cab <nc482@exeter.ac.uk>
2023-10-22 02:35:38 +0100

    react-w3: react hooks useContext completed

--- react-tutorial/learn-react/src/components/hooks/UseContext.jsx
+++ react-tutorial/learn-react/src/components/hooks/UseContext.jsx
@@ -1,4 +1,4 @@
-import { useState, createContext } from "react";
+import { useState, createContext, useContext } from "react";
 // React Context is a way to manage state globally
 // it can be used with the useState Hook to share between deeply nested components more easily than with useState alone
 
@@ -36,12 +36,45 @@ function Component3({ user }) {
 // --------------------------------------------------
 
 // a solution better than "prop drilling" is to create a context
-// import { createContext } from "react"
+// STEP1) import { createContext, useContext } from "react"
 
-// first initialze the context
+// STEP2) initialze the context
 const UserContext = createContext();
 
-function CreateContext() {}
+function CreateContext() {
+  const [user, setUser] = useState("Jesse Hall");
+
+  // STEP3) use the Context Provider to wrap the tree of components that need the state Context, and supply the state value "user"
+  // now all the components in this tree will have access to the user Context
+  // to access the state Context, see STEP4
+  return (
+    <UserContext.Provider value={user}>
+      <h1>Component 1</h1>
+      <h1>{`Hello ${user}!`}</h1>
+      <Component2Context />
+    </UserContext.Provider>
+  );
+}
+function Component2Context() {
+  return (
+    <>
+      <h1>Component 2</h1>
+      <Component3Context />
+    </>
+  );
+}
+function Component3Context() {
+  // to access the state Context at any level of the tree of components, instead of having to pass a value from level to level, a component can just access the value directly using the "useContext" hook and the context created at the beginning as the argument
+  const user = useContext(UserContext);
+  return (
+    <>
+      <h1>Component 3</h1>
+      <h2>{`Hello ${user} again!`}</h2>
+    </>
+  );
+}
+
+// ------------------------------
 
 const divStyle = {
   border: "solid",
@@ -59,6 +92,10 @@ function UseContext() {
       <h3 style={h3Style}>Prop Drilling example</h3>
       <div style={divStyle}>
         <PropDrilling />
+      </div>
+      <h3 style={h3Style}>Create Context example</h3>
+      <div style={divStyle}>
+        <CreateContext />
       </div>
     </>
   );
