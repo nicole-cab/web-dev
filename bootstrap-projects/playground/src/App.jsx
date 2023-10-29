3036e62ac16304d8ad5695886c96ea249a652db1
nicole-cab <nicole.cabaya@yahoo.com>
2023-10-29 03:45:43 +0000

    x-clone: init react project, found font - peter font

---
+++ bootstrap-projects/playground/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <h1>Hello</h1>
+      <div className="row">
+        <p className="col-6">hi</p>
+        <p className="col-6">hello</p>
+      </div>
+    </>
+  );
+}
+export default App;
---
+++ bootstrap-projects/x-clone/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+function App() {
+  return (
+    <>
+      <h1>𝕏-clone</h1>
+      <span className="display-name">Kris Bishop </span>
+      <span className="username">
+        <span className="at">@</span>KrisBishop87
+      </span>
+      <br />
+      <span className="post">Kiss the sport of</span>
+    </>
+  );
+}
+export default App;
